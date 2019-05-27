2bfe8b6792ef47f9529406549779b350d755fdd5
mumu <1050675545@qq.com>
2019-05-27 16:16:58 +0800

    count-to demo

---
+++ src/views/dashboard/admin/components/CountToUser.vue
@@ -0,0 +1,10 @@
+<template>
+  <count-to :startVal="1000" :endVal="13445555" :duration="3000"></count-to>
+</template>
+<script>
+import CountTo from 'vue-count-to';
+export default {
+  name: 'CountToUser',
+  components: { CountTo }
+}
+</script>
--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -2,12 +2,14 @@
   <div>
     admin
     <bar-chart></bar-chart>
+    <count-to-user></count-to-user>
   </div>
 </template>
 <script>
 import BarChart from './components/BarChart'
+import CountToUser from './components/CountToUser';
 export default {
   name: 'DashboardAdmin',
-  components: { BarChart }
+  components: { BarChart,CountToUser }
 }
 </script>
