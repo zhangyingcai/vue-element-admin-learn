13f69d9e1bebb60ee8f39681216a37152bb9a004
mumu <1050675545@qq.com>
2019-06-19 09:58:56 +0800

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -1,16 +1,21 @@
 const config = {
   tokentxuri: 'https://api.etherscan.io/api?module=account&action=tokentx',
   contractaddress: '0xfDEAA4ab9fea519aFD74df2257A21e5BcA0DFd3f',
+  tokenDecimal: 18,
   apikey: 'K7T921SQKMBW35UTUIVI5YU2VJX76IUQSV',
-  sort: 'desc',    // desc
   txlisturi: 'http://api.etherscan.io/api?module=account&action=txlist&',
   creatorAddress: '0x1F759FAE44ca006a496434908B009820afEa0A90',
-  offset: 10,
-  startTimestamp: 1531584000, // 2018-07-15 0:0:0
   tokenbalances: 'https://api.etherscan.io/api?module=account&action=balancemulti',
   tokeninternal: 'https//api.etherscan.io/api?module=account&action=txlistinternal',
   // 获取当前合约的 token 数量
-  tokenbalance:'https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0xfDEAA4ab9fea519aFD74df2257A21e5BcA0DFd3f&apikey=K7T921SQKMBW35UTUIVI5YU2VJX76IUQSV'
+  tokenbalance:'https://api.etherscan.io/api?module=stats&action=tokensupply&contractaddress=0xfDEAA4ab9fea519aFD74df2257A21e5BcA0DFd3f&apikey=K7T921SQKMBW35UTUIVI5YU2VJX76IUQSV',
+  apiurl: 'http://blockapi.liuyiqian.net:7001/holders', // 排行榜
+  acountinfourl: 'http://blockapi.liuyiqian.net:7001/accountinfo', // 单个地址
+  txinfourl: 'http://localhost:7001/txinfo', // 单个地址
+  // apiurl: 'http://localhost:7001/holders'
+  appeth: '0x06bb48363eff79604febc5329dd3688690e7fd8f', // 官方地址
+  otcbtceth: '0xaeec6f5aca72f3a005af1b3420ab8c8c7009bac8',
+  cleareth: '0x4660378743FA83f0d1F45d60FA9f5c9D1D7C1D49'
 }
 
 export default config
